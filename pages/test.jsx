b9957a088200319204241522c3b9d50a1843c817
Zahra <namazi.q@gmail.com>
2022-09-12 17:02:49 +0430

    add fun for apps

--- pages/test.jsx
+++ pages/test.jsx
@@ -2,7 +2,7 @@ import { useRouter } from 'next/router'
 import React, { useState } from 'react'
 import { Button, Flex } from 'rebass'
 import styled from 'styled-components'
-import { isBrowser } from 'react-device-detect'
+import { isBrowser, isAndroid } from 'react-device-detect'
 const Input = styled.input`
   padding: 15px;
   width: 100%;
@@ -11,13 +11,28 @@ const Input = styled.input`
 `
 const Label = styled.label``
 
+export const FACEBOOK_APP_IOS_BASE_URL = 'fb://profile/'
+export const FACEBOOK_APP_ANDROID_BASE_URL = 'fb://facewebmodal/f?href=https://www.facebook.com/'
+
+export const FACEBOOK = 'facebook'
+
 const Test = () => {
   const router = useRouter()
   const [alert, setAlert] = useState(false)
   const [url, setUrl] = useState('')
+  const [baseUrl, setBaseUrl] = useState('')
+
   const click = () => {
     router.push('card2')
     setAlert(true)
+  }
+
+  const getProperURL = (url, baseUrl) => {
+    return isBrowser
+      ? url
+      : baseUrl && baseUrl.includes(FACEBOOK)
+      ? url.replace(baseUrl, isAndroid ? FACEBOOK_APP_ANDROID_BASE_URL : FACEBOOK_APP_IOS_BASE_URL)
+      : url
   }
 
   return (
@@ -31,13 +46,14 @@ const Test = () => {
 
         <Input type="text" placeholder="Past URL" onChange={(e) => setUrl(e.currentTarget.value)} name="url" />
       </Flex>
+      <Flex flexDirection="column" width={`100%`}>
+        <Label htmlFor="url">Past Base URL</Label>
+
+        <Input type="text" placeholder="Past BASE URL" onChange={(e) => setBaseUrl(e.currentTarget.value)} name="url" />
+      </Flex>
       <Button backgroundColor="#2de7de" margin={`20px 0`}>
-        <a
-          href={isBrowser ? url : url.replace('https://www.facebook.com', 'fb://page')}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          {`link to ${isBrowser ? url : url.replace('https://www.facebook.com', 'fb://page')}`}
+        <a href={getProperURL(url, baseUrl)} target="_blank" rel="noopener noreferrer">
+          {`link to ${getProperURL(url, baseUrl)}`}
         </a>
       </Button>
       <a href="fb://profile/abc">abc IOS</a>
